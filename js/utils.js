bef491956ffcd581b0d16911fd82ea21537cc66b
ITsPorky <wc.walsh16@gmail.com>
2022-04-26 15:08:20 +1000

    Added rollDice() method

--- js/utils.js
+++ js/utils.js
@@ -46,6 +46,45 @@ const utils = {
     const event = new CustomEvent(name, { detail });
     document.dispatchEvent(event);
   },
+  // Dice Methods
+  rollDice(numberOfSides, numberOfRolls = 1) {
+    let total = 0;
+    for (let i = 0; i < numberOfRolls; i++) {
+      let roll = Math.floor(Math.random() * numberOfSides) + 1;
+      total += roll;
+    }
+    return total;
+  },
+  getRollType(value) {
+    switch (value) {
+      case "d3":
+        return rollDice(3);
+      case "d4":
+        return rollDice(4);
+      case "d6":
+        return rollDice(6);
+      case "d8":
+        return rollDice(8);
+      case "d10":
+        return rollDice(10);
+      case "d12":
+        return rollDice(12);
+      case "2d3":
+        return rollDice(3, 2);
+      case "3d3":
+        return rollDice(3, 3);
+      case "4d3":
+        return rollDice(3, 4);
+      case "2d4":
+        return rollDice(4, 2);
+      case "3d4":
+        return rollDice(4, 3);
+      case "4d4":
+        return rollDice(4, 4);
+      case "2d6":
+        return rollDice(6, 2);
+    }
+  },
 
   // Other methods for fetching API data
   async fetchData(seed) {
