4f184c2b27efcd9bcb6604961d1db4998a3e6f1a
ITsPorky <wc.walsh16@gmail.com>
2022-05-08 11:59:23 +1000

    Added Player state class

---
+++ state/PlayerState.js
@@ -0,0 +1,37 @@
+class PlayerState {
+  constructor(data) {
+    this.players = {
+      player1: {
+        ...Players.player1,
+        actions: [Players.player1.weapon.type],
+        maxHp: Players.player1.hp,
+        xp: 40,
+        maxXp: 50,
+        level: 1,
+        status: null,
+        isPlayerControlled: true,
+      },
+    };
+    this.lineup = ["player1"];
+    this.inventory = [
+      { actionId: "item_cureStatusPotion", instanceId: "p1" },
+      { actionId: "item_cureStatusPotion", instanceId: "p2" },
+      { actionId: "item_hpPotion", instanceId: "p4" },
+    ];
+  }
+
+  swapLineup(oldId, incomingId) {
+    const oldIndex = this.lineup.indexOf(oldId);
+    this.lineup[oldIndex] = incomingId;
+    utils.emitEvent("LineupChanged");
+  }
+
+  moveToFront(futureFrontId) {
+    this.lineup = this.lineup.filter(id !== futureFrontId);
+    this.lineup.unshift(futureFrontId);
+
+    utils.emitEvent("LineupChanged");
+  }
+}
+
+window.playerState = new PlayerState();
